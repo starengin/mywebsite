922cb6a095aeb907267f13bef957b3e88fd444a5
Ayan Ansari <corporate@stareng.co.in>
2026-03-02 14:05:55 +0530

    Fix build: remove missing Ledger route

--- apps/customer-portal/src/routes/index.jsx
+++ apps/customer-portal/src/routes/index.jsx
@@ -6,7 +6,6 @@ import ProtectedRoute from "../components/ProtectedRoute";
 import Login from "../pages/Login/Login.jsx";
 import AppShell from "../components/layout/AppShell.jsx";
 import Home from "../pages/Home/Home.jsx";
-import Ledger from "../pages/Ledger/Ledger.jsx";
 import Transactions from "../pages/Transactions/Transactions.jsx";
 
 import { isAuthed } from "../lib/auth.js";
@@ -32,7 +31,6 @@ export default function AppRoutes() {
         }
       >
         <Route index element={<Home />} />
-        <Route path="ledger" element={<Ledger />} />
         <Route path="transactions" element={<Transactions />} />
       </Route>
 
